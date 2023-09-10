8f803b957ce8b3cbab269dd0a19f0f7105b19702
Drew Medina <ofc10drew@gmail.com>
2023-09-10 14:11:14 -0500

    new color button

--- Checker/src/Checkers/gui/GameOverPanel.java
+++ Checker/src/Checkers/gui/GameOverPanel.java
@@ -16,12 +16,15 @@ public class GameOverPanel extends JPanel {
         setLayout(new GridLayout(100, 100));
         setBackground(Color.decode("#B6977D"));
 
-        gameOverMessage = new JLabel("GAME OVER " + color + " Wins");
+        gameOverMessage = new JLabel("GAME OVER " + color + " WINS");
         gameOverMessage.setFont(new Font("Times New Roman", Font.BOLD, 50));
 
         add(gameOverMessage);
 
         JButton restartButton = new JButton("New Game");
+        gameOverMessage.setFont(new Font("Helvetica Neue", Font.BOLD, 35));
+        restartButton.setBackground(Color.decode("#140a07")); // Set the background color
+        restartButton.setForeground(Color.WHITE);
         restartButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,6 +39,6 @@ public class GameOverPanel extends JPanel {
     public void setColor(PieceColor color) {
         this.color = color;
 
-        gameOverMessage.setText("GAME OVER\n " + color + "\n Wins");
+        gameOverMessage.setText("GAME OVER\n " + color + "\n WINS");
     }
 }
